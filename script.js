0fec95e1c39c044253a72213e5c92bdb10c30ac9
namyn05 <yasmindanieli95@gmail.com>
2024-03-21 22:05:51 -0300

    Atividade avaliativa

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickMenu () {
+    var itensMenu = document.getElementById('itens')
+   if (itensMenu.style.display = 'blok') {
+    itensMenu.style.display = 'none'
+   } else {
+    itensMenu.style.display = 'block'
+   }
+}
